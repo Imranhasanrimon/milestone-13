a845927c9e03b75190ce17890a6fd0bd7e450cae
Imranhasanrimon <imranhasanrimon5@gmail.com>
2025-03-06 22:10:13 +0600

    adding server side for pic-seek project

---
+++ conceptual-2/pic-seek-server/index.js
@@ -0,0 +1,12 @@
+require('dotenv').config()
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+    res.send({ message: 'This is the default API endpoint' })
+})
+app.listen(port, () => {
+    console.log('server is running on port', port);
+})
+
